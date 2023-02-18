10336cbd1246b7044745a33172e35f4b76bc92bd
Lucas Raoni <lucasraoniha@gmail.com>
2023-02-18 11:51:35 -0300

    Added some kind of broken gravity system

---
+++ README.md
@@ -0,0 +1,22 @@
+Bevy test ground
+================
+
+Testing:
+- N body simulation
+
+TODO:
+- Gravity module not working
+- Took inspiration from other code from WhoisDavid n body simulation
+- Can't make it work to the current version of Bevy
+
+### How to install and run it
+
+```bash
+# Clone the repo
+$ git clone https://github.com/mahauni/bevy-tinkering.git
+
+$ cd bevy-tinkering
+
+# Run it
+$ Cargo run
+```
--- src/main.rs
+++ src/main.rs
@@ -7,16 +7,11 @@ use bevy::{
     time::FixedTimestep,
 };
 
+mod plugins;
+use plugins::nbody::{BodyBundle, Gravity, NBody};
+
 // Defines the amount of time that should elapse between each physics step.
 const TIME_STEP: f32 = 1.0 / 60.0;
-
-// These constants are defined in `Transform` units.
-// Using the default 2D camera they correspond 1:1 with screen pixels.
-const PADDLE_SIZE: Vec3 = Vec3::new(120.0, 20.0, 0.0);
-const GAP_BETWEEN_PADDLE_AND_FLOOR: f32 = 60.0;
-const PADDLE_SPEED: f32 = 500.0;
-// How close can the paddle get to the wall
-const PADDLE_PADDING: f32 = 10.0;
 
 // We set the z-value of the ball to 1 so it renders on top in the case of overlapping sprites.
 const BALL_STARTING_POSITION: Vec3 = Vec3::new(0.0, -50.0, 1.0);
@@ -33,7 +28,6 @@ const BOTTOM_WALL: f32 = -300.;
 const TOP_WALL: f32 = 300.;
 
 const BACKGROUND_COLOR: Color = Color::rgb(0.9, 0.9, 0.9);
-const PADDLE_COLOR: Color = Color::rgb(0.3, 0.3, 0.7);
 const BALL_COLOR: Color = Color::rgb(1.0, 0.5, 0.5);
 const WALL_COLOR: Color = Color::rgb(0.8, 0.8, 0.8);
 
@@ -47,15 +41,11 @@ fn main() {
             SystemSet::new()
                 .with_run_criteria(FixedTimestep::step(TIME_STEP as f64))
                 .with_system(check_for_collisions)
-                .with_system(move_paddle.before(check_for_collisions))
                 .with_system(apply_velocity.before(check_for_collisions))
         )
         .add_system(bevy::window::close_on_esc)
         .run();
 }
-
-#[derive(Component)]
-struct Paddle;
 
 #[derive(Component)]
 struct Ball;
@@ -150,25 +140,14 @@ fn setup(
     // Camera
     commands.spawn(Camera2dBundle::default());
 
-    // Paddle
-    let paddle_y = BOTTOM_WALL + GAP_BETWEEN_PADDLE_AND_FLOOR;
 
-    commands.spawn((
-        SpriteBundle {
-            transform: Transform {
-                translation: Vec3::new(0.0, paddle_y, 0.0),
-                scale: PADDLE_SIZE,
-                ..default()
-            },
-            sprite: Sprite {
-                color: PADDLE_COLOR,
-                ..default()
-            },
-            ..default()
-        },
-        Paddle,
-        Collider,
-    ));
+    // Center Square
+    commands.spawn(MaterialMesh2dBundle {
+        mesh: meshes.add(Mesh::from(shape::Quad::default())).into(),
+        transform: Transform::default().with_scale(Vec3::splat(128.)),
+        material: materials.add(ColorMaterial::from(Color::PURPLE)),
+        ..default()
+    });
 
     // Ball
     commands.spawn((
@@ -189,31 +168,6 @@ fn setup(
     commands.spawn(WallBundle::new(WallLocation::Top)); 
 }
 
-fn move_paddle(
-    keyboard_input: Res<Input<KeyCode>>,
-    mut query: Query<&mut Transform, With<Paddle>>,
-) {
-    let mut paddle_transform = query.single_mut();
-    let mut direction = 0.0;
-
-    if keyboard_input.pressed(KeyCode::Left) {
-        direction -= 1.0;
-    }
-
-    if keyboard_input.pressed(KeyCode::Right) {
-        direction += 1.0;
-    }
-
-    // Calculate the new horizontal paddle position based on player input
-    let new_paddle_position = paddle_transform.translation.x + direction * PADDLE_SPEED * TIME_STEP;
-
-    // Update the paddle position,
-    // making sure it doesn't cause the paddle to leave the arena
-    let left_bound = LEFT_WALL + WALL_THICKNESS / 2.0 + PADDLE_SIZE.x / 2.0 + PADDLE_PADDING;
-    let right_bound = RIGHT_WALL - WALL_THICKNESS / 2.0 - PADDLE_SIZE.x / 2.0 - PADDLE_PADDING;
-
-    paddle_transform.translation.x = new_paddle_position.clamp(left_bound, right_bound);
-}
 
 fn apply_velocity(mut query: Query<(&mut Transform, &Velocity)>) {
     for (mut transform, velocity) in &mut query {
